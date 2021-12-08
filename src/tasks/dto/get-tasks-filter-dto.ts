f0acc605ec2660d0dc675bccd1bd82c42d8dced0
Ever Orellana <eorellana@applaudostudios.com>
2021-12-08 01:10:49 -0600

    chore: adding course of NesJs

---
+++ src/tasks/dto/get-tasks-filter-dto.ts
@@ -0,0 +1,12 @@
+import { IsOptional, IsEnum, IsString } from 'class-validator';
+import { TaskStatus } from '../task-status.enum';
+
+export class GetTasksFilterDto {
+  @IsOptional()
+  @IsEnum(TaskStatus)
+  status?: TaskStatus;
+
+  @IsOptional()
+  @IsString()
+  search?: string;
+}
---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task-dto';
+import { UpdateTaskStatusDto } from './dto/update-task-status-dto';
+import { TaskStatus } from './task-status.enum';
+import { Task } from './task.entity';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  // @Get()
+  // getAllTasks(): Task[] {
+  //   return this.tasksService.getAllTasks();
+  // }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Promise<Task> {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): Promise<void> {
+    return this.tasksService.deleteTask(id);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body() updateTaskStatusDto: UpdateTaskStatusDto,
+  ): Promise<Task> {
+    const { status } = updateTaskStatusDto;
+    return this.tasksService.updateTaskStatus(id, status);
+  }
+}
