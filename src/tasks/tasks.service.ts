2bdaf73122acb65eadbda49be40e9489ed3cacf3
Ever Orellana <eorellana@applaudostudios.com>
2021-12-09 18:42:51 -0600

    chore: adding user to all controllers

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -30,15 +30,19 @@ export class TasksService {
   createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
     return this.tasksRepository.createTask(createTaskDto, user);
   }
-  async deleteTask(id: string): Promise<void> {
-    const result = await this.tasksRepository.delete({ id });
+  async deleteTask(id: string, user: User): Promise<void> {
+    const result = await this.tasksRepository.delete({ id, user });
 
     if (result.affected === 0) {
       throw new NotFoundException(`Task with ID "${id}" not found`);
     }
   }
 
-  async updateTaskStatus(id: string, status: TaskStatus, user: User): Promise<Task> {
+  async updateTaskStatus(
+    id: string,
+    status: TaskStatus,
+    user: User,
+  ): Promise<Task> {
     const task = await this.getTaskById(id, user);
 
     task.status = status;
